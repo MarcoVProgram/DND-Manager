aa17721d224d94091c63f95f29ad809c16ba5ece
COMPUS021\daw1 <mvalienter2501@educantabria.es>
2026-04-16 09:10:29 +0200

    Revert "cambios pequeños detalles"
    This reverts commit 2bd0547d61af51f5903057b8dbb5fe5424a4f070.

--- DNDManager/src/main/java/manager/Main.java
+++ DNDManager/src/main/java/manager/Main.java
@@ -10,6 +10,7 @@ import manager.objects.abstractObjects.Tagged;
 import manager.tools.MyUtils;
 import manager.tools.Patterns;
 
+import java.awt.*;
 import java.io.*;
 import java.util.List;
 import java.util.Set;
@@ -255,7 +256,7 @@ public class Main {
 
         int id;
 
-        MyUtils.menuMaker("Select how do you want to identify the PC",
+        MyUtils.menuMaker("Select how do you want to identify the PC (Type ID -1 if empty)",
                 options);
 
         int choice = MyUtils.insertValidInt("Choose one of the two options",
